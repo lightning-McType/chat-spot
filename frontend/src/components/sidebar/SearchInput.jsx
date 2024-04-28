eaf1cf43dbf14d8854592cd46cd5da80b830d86a
lightning-McType <mohitsuri912@gmail.com>
2024-04-28 21:00:35 +0530

    complete conversations UI & function

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import { IoSearchSharp } from 'react-icons/io5';
+import useConversation from '../../zustand/useConversation';
+import useGetConversations from '../../hooks/useGetConversations';
+import toast from 'react-hot-toast';
 
 const SearchInput = () => {
+	const [search, setSearch] = useState('');
+	const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (!search) return;
+
+		if (search.length < 3) {
+			return toast.error('Search term must be 3 characters long');
+		}
+
+		const conversation = conversations.find((c) => c.fullname.toLowerCase().includes(search.toLocaleLowerCase()));
+
+		if (conversation) {
+			setSelectedConversation(conversation);
+			setSearch('');
+		} else {
+			toast.error('No such user found!');
+		}
+	};
 	return (
-		<form className="flex items-center gap-2">
+		<form
+			className="flex items-center gap-2"
+			onSubmit={handleSubmit}>
 			<input
 				type="text"
+				value={search}
 				placeholder="Search..."
 				className="input input-bordered rounded-full"
+				onChange={(e) => setSearch(e.target.value)}
 			/>
 			<button
 				type="Submit"
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,6 +1,29 @@
+import { Link } from 'react-router-dom';
 import GenderCheckbox from './GenderCheckbox';
+import { useState } from 'react';
+import useSignup from '../../hooks/useSignup';
 
 const Signup = () => {
+	const [inputs, setInputs] = useState({
+		fullname: '',
+		username: '',
+		password: '',
+		confirmPassword: '',
+		gender: '',
+	});
+
+	const { loading, signup } = useSignup();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		await signup(inputs);
+	};
+
+	const handleCheckboxChange = (gender) => {
+		setInputs({ ...inputs, gender });
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
 			<div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,13 +31,15 @@ const Signup = () => {
 					Sign Up <span className="text-blue-500">ChatSpot</span>
 				</h1>
 
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<label className="label p-2">
 							<span className="text-base label-text">Full Name</span>
 						</label>
 						<input
 							type="text"
+							value={inputs.fullname}
+							onChange={(e) => setInputs({ ...inputs, fullname: e.target.value })}
 							placeholder="John Doe"
 							className="w-full input input-bordered h-10"
 						/>
@@ -26,6 +51,8 @@ const Signup = () => {
 						</label>
 						<input
 							type="text"
+							value={inputs.username}
+							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
 							placeholder="johndoe"
 							className="w-full input input-bordered h-10"
 						/>
@@ -37,6 +64,8 @@ const Signup = () => {
 						</label>
 						<input
 							type="password"
+							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+							value={inputs.password}
 							placeholder="Enter password"
 							className="w-full input input-bordered h-10"
 						/>
@@ -47,21 +76,30 @@ const Signup = () => {
 						</label>
 						<input
 							type="password"
+							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
+							value={inputs.confirmPassword}
 							placeholder="Confirm Password"
 							className="w-full input input-bordered h-10"
 						/>
 					</div>
 
-					<GenderCheckbox />
+					<GenderCheckbox
+						onCheckboxChange={handleCheckboxChange}
+						selectedGender={inputs.gender}
+					/>
 
-					<a
-						href="#"
-						className="text-sm hover:underline hover:text-blue-600 inline-block">
+					<Link
+						to="/login"
+						className="text-sm hover:underline hover:text-blue-600 inline-block mt-2">
 						Already have an account?
-					</a>
+					</Link>
 
 					<div>
-						<button className="btn btn-block btn-info btn-sm mt-2 border text-white">Sign Up</button>
+						<button
+							className="btn btn-block btn-info btn-sm mt-2 border text-white"
+							disabled={loading}>
+							{loading ? <span className="loading loading-spinner" /> : 'Sign Up'}
+						</button>
 					</div>
 				</form>
 			</div>
