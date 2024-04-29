b0347f03f396a5eab894adbc0d7dd19894321036
lightning-McType <mohitsuri912@gmail.com>
2024-04-29 12:13:01 +0530

    socket URL updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io('http://localhost:8080', {
+			const socket = io('https://chat-spot-prod.onrender.com', {
 				query: {
 					userId: authUser._id,
 				},
