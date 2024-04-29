8d07fc86110d5fc110c873d665b5e7d3be092140
lightning-McType <mohitsuri912@gmail.com>
2024-04-29 11:44:11 +0530

    deploy to render

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
@@ -13,6 +14,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8081;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,9 +23,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', usersRoutes);
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello World!');
-// });
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
 	connectToDB();
